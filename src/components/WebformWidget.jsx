804c56825b32592b9e04ff3f7bf39cb764140147
github_christiandc19 <christiandc19@hotmail.com>
2026-05-08 09:27:45 -0700

    Fix webform API key

--- src/components/WebformWidget.jsx
+++ src/components/WebformWidget.jsx
@@ -12,7 +12,7 @@ window.WebSmartAssistantForm({
   clientKey: "evergreen-heights",
   formKey: "senior-living-contact",
   apiUrl: "https://su3cjmqk2h.ap-southeast-2.awsapprunner.com/api/Leads",
-  apiKey: "dev-webform-key-123",
+  apiKey: "dev-webform-key-12345",
   source: "webform",
 });
 
